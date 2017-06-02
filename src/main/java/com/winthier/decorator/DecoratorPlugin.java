b071c39f2184853562376a05a9fa140e1ae1c383
StarTux <ths.loy@googlemail.com>
2017-06-02 20:14:56 +0200

    Simpler, more reliable algorithm

--- src/main/java/com/winthier/decorator/DecoratorPlugin.java
+++ src/main/java/com/winthier/decorator/DecoratorPlugin.java
@@ -1,98 +1,181 @@
 package com.winthier.decorator;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
+import lombok.Value;
+import org.bukkit.Chunk;
+import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
-public class DecoratorPlugin extends JavaPlugin {
-    final Map<UUID, BukkitRunnable> tasks = new HashMap<>();
+public final class DecoratorPlugin extends JavaPlugin {
+    private Set<Vec> todo;
+    private int total;
+    private boolean paused;
+    private final Map<UUID, Vec> anchors = new HashMap<>();
+
+    @Value
+    final class Vec {
+        private final int x, z;
+    }
+
+    @Override
+    public void onEnable() {
+        reloadConfig();
+        int interval = getConfig().getInt("interval");
+        getLogger().info("Interval " + interval);
+        getServer().getScheduler().runTaskTimer(this, () -> onTick(), interval, interval);
+        loadTodo();
+    }
+
+    @Override
+    public void onDisable() {
+        saveTodo();
+    }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         final Player player = sender instanceof Player ? (Player)sender : null;
-        if (player == null) return false;
+        String cmd = args.length == 0 ? null : args[0].toLowerCase();
         if (args.length == 0) {
             return false;
-        } else if (args.length == 1 && ("cancel".equalsIgnoreCase(args[0]) ||
-                                        "stop".equalsIgnoreCase(args[0]))) {
-            BukkitRunnable task = tasks.remove(player.getUniqueId());
-            if (task == null) {
-                sender.sendMessage("No job running");
-            } else {
-                try {
-                    task.cancel();
-                } catch (IllegalStateException ise) {}
-                sender.sendMessage("Job cancelled");
+        }
+        if (cmd.equals("init") && args.length == 1) {
+            World world = getServer().getWorlds().get(0);
+            Location center = world.getWorldBorder().getCenter();
+            double size = world.getWorldBorder().getSize();
+            if (size > 100000) {
+                sender.sendMessage("World border too large!");
+                return true;
             }
-        } else if (args.length >= 2 && args.length <= 5) {
-            final int size = Integer.parseInt(args[0]);
-            final int interval = Integer.parseInt(args[1]);
-            final int stepX = args.length >= 3 ? Integer.parseInt(args[2]) : 128;
-            final int stepZ = args.length >= 4 ? Integer.parseInt(args[3]) : 128;
-            final DecoratorPlugin plugin = this;
-            final boolean down;
-            final int STEP;
-            if (stepX < 0) {
-                down = true;
-                STEP = -stepX;
-            } else {
-                down = false;
-                STEP = stepX;
-            }
-            final int STEPZ = Math.abs(stepZ);
-            final int sz = args.length >= 4 ? Integer.parseInt(args[4]) : (down ? size - STEPZ : -size + STEPZ);
-            BukkitRunnable task;
-            task = tasks.remove(player.getUniqueId());
-            if (task != null) {
-                try {
-                    task.cancel();
-                } catch (IllegalStateException ise) {}
-            }
-            task = new BukkitRunnable() {
-                int x = -size + STEP;
-                int z = sz;
-                @Override public void run() {
-                    if (!player.isValid()) {
-                        cancel();
-                        return;
-                    }
-                    World world = player.getWorld();
-                    Block block = world.getHighestBlockAt(x, z);
-                    player.teleport(block.getLocation().add(0.5, 0.5, 0.5));
-                    x += STEP;
-                    if (x > size - STEP) {
-                        x = -size + STEP;
-                        world.save();
-                        if (down) {
-                            z -= STEPZ;
-                            if (z < -size + STEPZ) {
-                                cancel();
-                                plugin.getLogger().info("Finis.");
-                                return;
-                            }
-                        } else {
-                            z += STEPZ;
-                            if (z > size - STEPZ) {
-                                cancel();
-                                plugin.getLogger().info("Finis.");
-                                return;
-                            }
-                        }
-                        plugin.getLogger().info(player.getName() + " z=" + z);
-                    }
+            Chunk min = center.add(size * -0.5, 0, size * -0.5).getChunk();
+            Chunk max = center.add(size * 0.5, 0, size * 0.5).getChunk();
+            todo = new HashSet<>();
+            for (int z = min.getZ() + 1; z < max.getZ(); z += 1) {
+                for (int x = min.getX() + 1; x < max.getX(); x += 1) {
+                    todo.add(new Vec(x, z));
                 }
-            };
-            tasks.put(player.getUniqueId(), task);
-            task.runTaskTimer(this, interval, interval);
-            sender.sendMessage("Job started. Size=" + size + " Interval=" + interval + " StepX=" + stepX + " StepZ=" + stepZ);
+            }
+            total = todo.size();
+            sender.sendMessage("" + todo.size() + " chunks scheduled.");
+        } else if (cmd.equals("pause") && args.length == 1) {
+            paused = !paused;
+            if (paused) {
+                sender.sendMessage("Decorator paused");
+            } else {
+                sender.sendMessage("Decorator unpaused");
+            }
+        } else if (cmd.equals("reload") && args.length == 1) {
+            loadTodo();
+            sender.sendMessage("Reloaded");
+        } else if (cmd.equals("cancel") && args.length == 1) {
+            todo = null;
+            saveTodo();
+            sender.sendMessage("Canceled");
+        } else if (cmd.equals("info") && args.length == 1) {
+            if (todo == null) {
+                sender.sendMessage("Not active");
+            } else {
+                int done = total - todo.size();
+                int percent = done * 100 / total;
+                sender.sendMessage(String.format("%d/%d Chunks done (%d%%)", done, total, percent));
+            }
+            if (paused) sender.sendMessage("Paused");
+        } else if (cmd.equals("save") && args.length == 1) {
+            saveTodo();
+            sender.sendMessage("Saved");
+        } else {
+            return false;
         }
         return true;
+    }
+
+    void loadTodo() {
+        todo = null;
+        YamlConfiguration config = YamlConfiguration.loadConfiguration(new File(getDataFolder(), "todo.yml"));
+        if (!config.isSet("todo")) return;
+        todo = new HashSet<>();
+        Iterator<Integer> iter = config.getIntegerList("todo").iterator();
+        while (iter.hasNext()) {
+            todo.add(new Vec(iter.next(), iter.next()));
+        }
+        total = config.getInt("total");
+        getLogger().info("" + todo.size() + " todos loaded");
+    }
+
+    void saveTodo() {
+        YamlConfiguration config = new YamlConfiguration();
+        if (todo != null) {
+            config.set("total", total);
+            ArrayList<Integer> ls = new ArrayList<>();
+            for (Vec vec: todo) {
+                ls.add(vec.x);
+                ls.add(vec.z);
+            }
+            config.set("todo", ls);
+        }
+        try {
+            config.save(new File(getDataFolder(), "todo.yml"));
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    void onTick() {
+        if (paused || todo == null || todo.isEmpty()) return;
+        World world = getServer().getWorlds().get(0);
+        for (Player player: getServer().getOnlinePlayers()) {
+            Vec anchor = anchors.get(player.getUniqueId());
+            if (anchor == null) {
+                Chunk chunk = player.getLocation().getChunk();
+                anchor = new Vec(chunk.getX(), chunk.getZ());
+                anchors.put(player.getUniqueId(), anchor);
+            }
+            int chunkX = anchor.x;
+            int chunkZ = anchor.z;
+            Vec nextVec = null;
+            int minDist = 0;
+            for (Vec vec: todo) {
+                int dist = Math.max(Math.abs(vec.x - chunkX),
+                                    Math.abs(vec.z - chunkZ));
+                if (nextVec == null || minDist > dist) {
+                    nextVec = vec;
+                    minDist = dist;
+                }
+            }
+            if (minDist > 4) {
+                anchors.put(player.getUniqueId(), nextVec);
+            }
+            todo.remove(nextVec);
+            int x = nextVec.x * 16 + 8;
+            int z = nextVec.z * 16 + 8;
+            Location location = world.getHighestBlockAt(x, z).getLocation();
+            Location playerLocation = player.getLocation();
+            location.setYaw(playerLocation.getYaw());
+            location.setPitch(playerLocation.getPitch());
+            player.teleport(location);
+            if (todo.size() % 1000 == 0) {
+                int done = total - todo.size();
+                int percent = done * 100 / total;
+                getLogger().info(String.format("%d/%d Chunks done (%d%%)", done, total, percent));
+                saveTodo();
+                world.save();
+            }
+        }
+        if (todo.isEmpty()) {
+            todo = null;
+            getLogger().info("Done!");
+        }
     }
 }
