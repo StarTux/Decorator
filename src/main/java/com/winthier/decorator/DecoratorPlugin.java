ee8269f58e196305161e74ade7010838fcc0b7bf
StarTux <ths.loy@googlemail.com>
2018-09-01 13:46:05 +0200

    Spawn fake players

--- src/main/java/com/winthier/decorator/DecoratorPlugin.java
+++ src/main/java/com/winthier/decorator/DecoratorPlugin.java
@@ -1,8 +1,45 @@
 package com.winthier.decorator;
 
+import com.github.steveice10.mc.auth.data.GameProfile;
+import com.github.steveice10.mc.auth.exception.request.RequestException;
+import com.github.steveice10.mc.protocol.MinecraftConstants;
+import com.github.steveice10.mc.protocol.MinecraftProtocol;
+import com.github.steveice10.mc.protocol.ServerLoginHandler;
+import com.github.steveice10.mc.protocol.data.SubProtocol;
+import com.github.steveice10.mc.protocol.data.game.entity.player.GameMode;
+import com.github.steveice10.mc.protocol.data.game.setting.Difficulty;
+import com.github.steveice10.mc.protocol.data.game.world.WorldType;
+import com.github.steveice10.mc.protocol.data.message.ChatColor;
+import com.github.steveice10.mc.protocol.data.message.ChatFormat;
+import com.github.steveice10.mc.protocol.data.message.Message;
+import com.github.steveice10.mc.protocol.data.message.MessageStyle;
+import com.github.steveice10.mc.protocol.data.message.TextMessage;
+import com.github.steveice10.mc.protocol.data.message.TranslationMessage;
+import com.github.steveice10.mc.protocol.data.status.PlayerInfo;
+import com.github.steveice10.mc.protocol.data.status.ServerStatusInfo;
+import com.github.steveice10.mc.protocol.data.status.VersionInfo;
+import com.github.steveice10.mc.protocol.data.status.handler.ServerInfoBuilder;
+import com.github.steveice10.mc.protocol.data.status.handler.ServerInfoHandler;
+import com.github.steveice10.mc.protocol.data.status.handler.ServerPingTimeHandler;
+import com.github.steveice10.mc.protocol.packet.ingame.client.ClientChatPacket;
+import com.github.steveice10.mc.protocol.packet.ingame.server.ServerChatPacket;
+import com.github.steveice10.mc.protocol.packet.ingame.server.ServerJoinGamePacket;
+import com.github.steveice10.packetlib.Client;
+import com.github.steveice10.packetlib.Server;
+import com.github.steveice10.packetlib.Session;
+import com.github.steveice10.packetlib.event.server.ServerAdapter;
+import com.github.steveice10.packetlib.event.server.ServerClosedEvent;
+import com.github.steveice10.packetlib.event.server.SessionAddedEvent;
+import com.github.steveice10.packetlib.event.server.SessionRemovedEvent;
+import com.github.steveice10.packetlib.event.session.DisconnectedEvent;
+import com.github.steveice10.packetlib.event.session.PacketReceivedEvent;
+import com.github.steveice10.packetlib.event.session.SessionAdapter;
+import com.github.steveice10.packetlib.tcp.TcpSessionFactory;
 import java.io.File;
 import java.io.IOException;
+import java.net.Proxy;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -10,6 +47,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 import lombok.Value;
+import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.World;
@@ -28,6 +66,10 @@ public final class DecoratorPlugin extends JavaPlugin implements Listener {
     private boolean paused, debug;
     private final Map<UUID, Vec> anchors = new HashMap<>();
     private final Set<UUID> populateDidHappen = new HashSet<>();
+    private World world;
+    private String worldName;
+    private int interval;
+    private int fakePlayers, fakeCount = (int)System.nanoTime() % 10000, fakeCooldown;
 
     @Value
     final class Vec {
@@ -38,7 +80,8 @@ public final class DecoratorPlugin extends JavaPlugin implements Listener {
     public void onEnable() {
         reloadConfig();
         saveDefaultConfig();
-        int interval = getConfig().getInt("interval");
+        interval = getConfig().getInt("interval");
+        fakePlayers = getConfig().getInt("fake-players");
         getLogger().info("Interval " + interval);
         getServer().getScheduler().runTaskTimer(this, () -> onTick(), interval, interval);
         loadTodo();
@@ -57,58 +100,97 @@ public final class DecoratorPlugin extends JavaPlugin implements Listener {
         if (args.length == 0) {
             return false;
         }
-        if (cmd.equals("init") && args.length == 1) {
-            World world = getServer().getWorlds().get(0);
-            double radius = world.getWorldBorder().getSize() * 0.5;
-            if (radius > 100000) {
-                sender.sendMessage("World border too large!");
+        switch (cmd) {
+        case "init":
+            if (args.length == 1 || args.length == 2) {
+                if (args.length >= 2) {
+                    world = getServer().getWorld(args[1]);
+                    worldName = world.getName();
+                } else {
+                    world = getServer().getWorlds().get(0);
+                    worldName = world.getName();
+                }
+                double radius = world.getWorldBorder().getSize() * 0.5;
+                if (radius > 100000) {
+                    sender.sendMessage("World border too large!");
+                    return true;
+                }
+                Chunk min = world.getWorldBorder().getCenter().add(-radius, 0, -radius).getChunk();
+                Chunk max = world.getWorldBorder().getCenter().add(radius, 0, radius).getChunk();
+                todo = new HashSet<>();
+                for (int z = min.getZ() + 1; z < max.getZ(); z += 1) {
+                    for (int x = min.getX() + 1; x < max.getX(); x += 1) {
+                        todo.add(new Vec(x, z));
+                    }
+                }
+                total = todo.size();
+                sender.sendMessage("" + todo.size() + " chunks scheduled.");
                 return true;
             }
-            Chunk min = world.getWorldBorder().getCenter().add(-radius, 0, -radius).getChunk();
-            Chunk max = world.getWorldBorder().getCenter().add(radius, 0, radius).getChunk();
-            todo = new HashSet<>();
-            for (int z = min.getZ() + 1; z < max.getZ(); z += 1) {
-                for (int x = min.getX() + 1; x < max.getX(); x += 1) {
-                    todo.add(new Vec(x, z));
+            break;
+        case "pause":
+            if (args.length == 1) {
+                paused = !paused;
+                if (paused) {
+                    sender.sendMessage("Decorator paused");
+                } else {
+                    sender.sendMessage("Decorator unpaused");
                 }
+                return true;
             }
-            total = todo.size();
-            sender.sendMessage("" + todo.size() + " chunks scheduled.");
-        } else if (cmd.equals("pause") && args.length == 1) {
-            paused = !paused;
-            if (paused) {
-                sender.sendMessage("Decorator paused");
-            } else {
-                sender.sendMessage("Decorator unpaused");
+            break;
+        case "debug":
+            if (args.length == 1) {
+                debug = !debug;
+                if (debug) {
+                    sender.sendMessage("Debug mode enabled");
+                } else {
+                    sender.sendMessage("Debug mode disabled");
+                }
+                return true;
             }
-        } else if (cmd.equals("debug") && args.length == 1) {
-            debug = !debug;
-            if (debug) {
-                sender.sendMessage("Debug mode enabled");
-            } else {
-                sender.sendMessage("Debug mode disabled");
+            break;
+        case "reload":
+            if (args.length == 1) {
+                loadTodo();
+                sender.sendMessage("Reloaded");
+                return true;
             }
-        } else if (cmd.equals("reload") && args.length == 1) {
-            loadTodo();
-            sender.sendMessage("Reloaded");
-        } else if (cmd.equals("cancel") && args.length == 1) {
-            todo = null;
-            saveTodo();
-            sender.sendMessage("Canceled");
-        } else if (cmd.equals("info") && args.length == 1) {
-            if (todo == null) {
-                sender.sendMessage("Not active");
-            } else {
-                int done = total - todo.size();
-                int percent = done * 100 / total;
-                sender.sendMessage(String.format("%d/%d Chunks done (%d%%)", done, total, percent));
+            break;
+        case "cancel":
+            if (args.length == 1) {
+                todo = null;
+                saveTodo();
+                sender.sendMessage("Canceled");
+                return true;
             }
-            if (paused) sender.sendMessage("Paused");
-        } else if (cmd.equals("save") && args.length == 1) {
-            saveTodo();
-            sender.sendMessage("Saved");
-        } else {
-            return false;
+            break;
+        case "info":
+            if (args.length == 1) {
+                if (todo == null) {
+                    sender.sendMessage("Not active");
+                } else {
+                    int done = total - todo.size();
+                    int percent = done * 100 / total;
+                    sender.sendMessage(String.format("%d/%d Chunks done (%d%%)", done, total, percent));
+                }
+                if (paused) sender.sendMessage("Paused");
+                return true;
+            }
+            break;
+        case "save":
+            if (args.length == 1) {
+                saveTodo();
+                sender.sendMessage("Saved");
+                return true;
+            }
+            break;
+        case "fake":
+            if (args.length == 2) {
+                spawnFakePlayer(args[1]);
+                sender.sendMessage("Fake user logged in.");
+                return true;
+            }
         }
         return true;
     }
@@ -122,6 +204,7 @@ public final class DecoratorPlugin extends JavaPlugin implements Listener {
         while (iter.hasNext()) {
             todo.add(new Vec(iter.next(), iter.next()));
         }
+        worldName = config.getString("world");
         total = config.getInt("total");
         getLogger().info("" + todo.size() + " todos loaded");
     }
@@ -129,6 +212,7 @@ public final class DecoratorPlugin extends JavaPlugin implements Listener {
     void saveTodo() {
         YamlConfiguration config = new YamlConfiguration();
         if (todo != null) {
+            config.set("world", world.getName());
             config.set("total", total);
             ArrayList<Integer> ls = new ArrayList<>();
             for (Vec vec: todo) {
@@ -146,8 +230,14 @@ public final class DecoratorPlugin extends JavaPlugin implements Listener {
 
     void onTick() {
         if (paused || todo == null) return;
-        World world = getServer().getWorlds().get(0);
-        for (Player player: world.getPlayers()) {
+        if (world == null && worldName == null) return;
+        if (world == null) world = getServer().getWorld(worldName);
+        if (fakeCooldown <= 0 && getServer().getOnlinePlayers().size() < fakePlayers) {
+            spawnFakePlayer("fake" + fakeCount++);
+            fakeCooldown = 20;
+        }
+        if (fakeCooldown > 0) fakeCooldown -= interval;
+        for (Player player: getServer().getOnlinePlayers()) {
             if (todo.isEmpty()) {
                 todo = null;
                 getLogger().info("Done!");
@@ -196,7 +286,8 @@ public final class DecoratorPlugin extends JavaPlugin implements Listener {
     @EventHandler
     public void onChunkPopulate(ChunkPopulateEvent event) {
         if (todo == null) return;
-        World world = getServer().getWorlds().get(0);
+        if (world == null && worldName == null) return;
+        if (world == null) world = getServer().getWorld(worldName);
         if (!world.equals(event.getWorld())) return;
         Vec vec = new Vec(event.getChunk().getX(), event.getChunk().getZ());
         if (!todo.remove(vec)) return;
@@ -226,5 +317,30 @@ public final class DecoratorPlugin extends JavaPlugin implements Listener {
         int done = total - todo.size();
         int percent = done * 100 / total;
         getLogger().info(String.format("%d/%d Chunks done (%d%%)", done, total, percent));
+    }
+
+    // --- MCProtocolLib stuff
+
+    private void spawnFakePlayer(final String username) {
+        MinecraftProtocol protocol = new MinecraftProtocol(username);
+        Client client = new Client("localhost", Bukkit.getPort(), protocol, new TcpSessionFactory(Proxy.NO_PROXY));
+        client.getSession().setFlag(MinecraftConstants.AUTH_PROXY_KEY, Proxy.NO_PROXY);
+        client.getSession().addListener(new SessionAdapter() {
+            @Override
+            public void packetReceived(PacketReceivedEvent event) {
+                if(event.getPacket() instanceof ServerJoinGamePacket) {
+                    event.getSession().send(new ClientChatPacket(username + " says hello."));
+                }
+            }
+            @Override
+            public void disconnected(DisconnectedEvent event) {
+                System.out.println("Disconnected: " + Message.fromString(event.getReason()).getFullText());
+                if(event.getCause() != null) {
+                    event.getCause().printStackTrace();
+                }
+            }
+        });
+
+        client.getSession().connect();
     }
 }
