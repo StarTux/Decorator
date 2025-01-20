28f8172952b6a83f2364cfa0a8db7cb87acccb27
StarTux <ths.loy@gmail.com>
2025-01-20 17:17:21 +0100

    1.21.4: New client session construction

--- src/main/java/com/winthier/decorator/MCProtocolLib.java
+++ src/main/java/com/winthier/decorator/MCProtocolLib.java
@@ -1,11 +1,13 @@
 package com.winthier.decorator;
 
+import java.net.InetSocketAddress;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.geysermc.mcprotocollib.network.ClientSession;
 import org.geysermc.mcprotocollib.network.event.session.ConnectedEvent;
 import org.geysermc.mcprotocollib.network.event.session.DisconnectedEvent;
 import org.geysermc.mcprotocollib.network.event.session.SessionAdapter;
-import org.geysermc.mcprotocollib.network.tcp.TcpClientSession;
+import org.geysermc.mcprotocollib.network.factory.ClientNetworkSessionFactory;
 import org.geysermc.mcprotocollib.protocol.MinecraftProtocol;
 
 final class MCProtocolLib {
@@ -14,7 +16,11 @@ final class MCProtocolLib {
         final String host = "localhost";
         final int port = Bukkit.getPort();
         plugin.getLogger().info("Spawn fake player: " + host + ":" + port);
-        TcpClientSession client = new TcpClientSession(host, port, protocol); // no proxy
+        ClientSession client = ClientNetworkSessionFactory.factory()
+            .setRemoteSocketAddress(new InetSocketAddress(host, port))
+            .setProtocol(protocol)
+            .setProxy(null) // no proxy
+            .create();
         client.addListener(new SessionAdapter() {
             @Override
             public void connected(ConnectedEvent event) {
